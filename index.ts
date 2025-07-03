545b3c8257cc3dc959efc627dbd344c55d4436cf
jasper-elimity <jasper@elimity.com>
2025-07-03 09:16:46 +0200

    Convert Protobuf values to JSON

--- index.ts
+++ index.ts
@@ -1,23 +1,25 @@
 import { ConnectError, ConnectRouter } from "@connectrpc/connect";
+import { JsonValue, PlainMessage, Value } from "@bufbuild/protobuf";
 import {
   PerformImportRequest,
   PerformImportResponse,
 } from "./gen/elimity/insights/customgateway/v1alpha1/customgateway_pb.js";
-import { PlainMessage, Value } from "@bufbuild/protobuf";
 import { Service } from "./gen/elimity/insights/customgateway/v1alpha1/customgateway_connect.js";
 import { connectNodeAdapter } from "@connectrpc/connect-node";
 import { createServer } from "node:http";
+import { mapValues } from "lodash";
 
 export function serveGateway(
   handler: (
-    fields: Record<string, Value>,
+    fields: Record<string, JsonValue>,
   ) => AsyncGenerator<PlainMessage<PerformImportResponse>>,
 ): void {
   async function* generateResponses(
     request: PerformImportRequest,
   ): AsyncGenerator<PlainMessage<PerformImportResponse>> {
+    const fields = mapValues(request.fields, makeJsonValue);
     try {
-      yield* handler(request.fields);
+      yield* handler(fields);
     } catch (error) {
       throw ConnectError.from(error);
     }
@@ -28,4 +30,8 @@ export function serveGateway(
   const options = { routes: processRouter };
   const adapter = connectNodeAdapter(options);
   createServer(adapter).listen(80);
+}
+
+function makeJsonValue(value: Value): JsonValue {
+  return value.toJson();
 }
